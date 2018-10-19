33d1f79e0d17ab3ccf50462fa4c0f8aad6743e72
Matvey Glazkov <feed4rz@yandex.ru>
2018-10-19 15:26:50 +0300

    Benchmark logs

--- examples/inventory.js
+++ examples/inventory.js
@@ -3,9 +3,11 @@ const manager = new OpskinsManager({ apikey: process.env.apikey, polling: false 
 
 async function getInventory() {
   try {
+  	console.time('inventory')
   	const inventory = await manager.IUser.GetInventory({ app_id: 1, RECURSIVE: true })
+  	console.timeEnd('inventory')
 
-  	console.log(JSON.stringify(inventory))
+  	console.log(inventory.length)
   } catch(err) {
     console.error(err)
   }
